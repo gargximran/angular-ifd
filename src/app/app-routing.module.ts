b7c820474a3bb30f4bc4053212fe11cd3d1b7345
gargximran <gargximran@gmail.com>
2020-11-12 16:20:29 +0600

    create auth service and guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { AuthGuard } from './auth.guard'
-import { NotauthGuard } from './notauth.guard';
+
 
 const routes: Routes = [
   {
@@ -12,18 +11,15 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
-    canActivate: [AuthGuard],
     loadChildren: ()=> import('./admin/admin.module').then(m => m.AdminModule)
   },
   {
     path: 'login',
-    canActivate: [NotauthGuard],
     component: LoginComponent,
     
   },
   {
     path: "register",
-    canActivate: [NotauthGuard],
     component: RegisterComponent
     
   }
