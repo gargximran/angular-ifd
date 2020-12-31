36606e952d65a2e274338ba4277e10410166d25a
gargximran <gargximran@gmail.com>
2020-12-31 13:32:26 +0600

    evn

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,2 +1,2 @@
-export const url = 'http://127.0.0.1:5000';
-// export const url = 'http://68.183.87.197:5000';
+// export const url = 'http://127.0.0.1:5000';
+export const url = 'http://68.183.87.197:5000';
